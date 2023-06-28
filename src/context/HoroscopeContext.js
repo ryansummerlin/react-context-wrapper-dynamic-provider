ed862c70edef7e1c602499d8394c551917e187b8
Ryan Summerlin <ryansummerlin777@gmail.com>
2023-06-28 02:10:00 +0000

    Update 4 files

--- src/context/HoroscopeContext.js
+++ src/context/HoroscopeContext.js
@@ -1,3 +1,16 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
+import { default as horoscopeObj } from '../data/horoscopes';
 
 export const HoroscopeContext = createContext();
+
+const HoroscopeProvider = props => {
+  const [currentSign, setCurrentSign] = useState('Libra');
+  const sign = horoscopeObj[currentSign];
+  return (
+    <HoroscopeContext.Provider value={{ sign, setCurrentSign }}>
+      {props.children}
+    </HoroscopeContext.Provider>
+  );
+};
+
+export default HoroscopeProvider;
